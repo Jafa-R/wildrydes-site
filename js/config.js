39ea635daf372ef60d75b7698102cfe96aa370dd
Karam-Massad <56916782+Karam-Massad@users.noreply.github.com>
2022-01-29 03:31:03 +0200

    Update config.js
    w

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'sa-east-1_wzkpdlOcg', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4j3hf9ouu2thgs0jlk7h35tdbl', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'sa-east-1' // e.g. us-east-2
+        userPoolId: 'us-east-1_gSz6ll8KD', // e.g. us-east-1_gSz6ll8KD
+        userPoolClientId: '49ira5m46sdtucrm02ld9iftgk', // e.g. 49ira5m46sdtucrm02ld9iftgk
+        region: 'us-east-1' // e.g. us-east-1
     },
     api: {
         invokeUrl: 'https://an2eirbqsk.execute-api.sa-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
